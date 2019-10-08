09506a3c02f72f450d4fe9bbfd235b4912ce6ccb
Alec Kozakowski <AlecKozakowski@gmail.com>
2019-10-07 19:13:36 -0500

    initial commit of basic flask server

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world"
+
+@app.route('/mike')
+def mike():
+    return "hello mike"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
